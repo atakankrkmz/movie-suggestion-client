213ad666857b684a4f7516d1ccaf36d4c6a02228
otokon58 <ata58kann@gmail.com>
2021-06-14 17:37:30 +0300

    movie detail

--- src/components/pages/MovieDetailpage/index.jsx
+++ src/components/pages/MovieDetailpage/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 /* Dependencies */
 // import env from "react-dotenv";
 import axios from "axios";
@@ -7,61 +7,53 @@ import { Figure, Row, Col } from "react-bootstrap";
 /* Components */
 import Comments from "../../Comment/Comments.jsx";
 import AddComment from "../../Comment/AddComment.jsx";
+import { useParams } from "react-router";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export default class MovieDetailpage extends Component {
-  _isMounted = false;
-  constructor(...props) {
-    super(...props);
+const MovieDetailpage = (props) => {
+  const [movies, setMovies] = [];
+  const { id } = useParams();
 
-    this.state = {
-      movie: {},
-    };
-  }
-
-  componentDidMount = async () => {
-    await axios
-      .get(`${API_URL}api/Movies/getbyid?id=${this.props.match.params.id}`)
-      .then((res) => this.setState({ movie: res.data }))
+  useEffect(() => {
+    axios
+      .get(`${API_URL}api/Movies/getbyid?id=${id}`)
+      .then((res) => setMovies(res.data))
       .catch((err) => console.log(err));
-  };
+  }, []);
 
-  componentWillUnmount() {
-    this._isMounted = false;
-  }
-  render() {
-    const { name, description, poster } = this.state.movie;
-    return (
-      <div>
-        <Row>
-          <Col md="4" className="mx-3">
-            <Figure>
-              <Figure.Image
-                width={250}
-                alt="Movie Poster Image"
-                src={
-                  poster
-                    ? `${API_URL}uploads/moviecontent/posters/${poster}`
-                    : null
-                }
-              />
-            </Figure>
-          </Col>
-          <Col md="7">
-            <h3> {name}</h3>
-            <br />
-            <p>{description}</p>
-          </Col>
-        </Row>
-        <hr />
-        <Row>
-          <Col className="mx-5">
-            <Comments key={uuidv4()} movieid={this.props.match.params.id} />
-            <AddComment key={uuidv4()} movieid={this.props.match.params.id} />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
-}
+  const { name, description, poster } = this.state.movie;
+  return (
+    <div>
+      <Row>
+        <Col md="4" className="mx-3">
+          <Figure>
+            <Figure.Image
+              width={250}
+              alt="Movie Poster Image"
+              src={
+                poster
+                  ? `${API_URL}uploads/moviecontent/posters/${poster}`
+                  : null
+              }
+            />
+          </Figure>
+        </Col>
+        <Col md="7">
+          <h3> {name}</h3>
+          <br />
+          <p>{description}</p>
+        </Col>
+      </Row>
+      <hr />
+      <Row>
+        <Col className="mx-5">
+          <Comments key={uuidv4()} movieid={this.props.match.params.id} />
+          <AddComment key={uuidv4()} movieid={this.props.match.params.id} />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default MovieDetailpage;
