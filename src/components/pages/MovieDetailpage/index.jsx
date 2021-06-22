e0ddec66a8dff3a2f6b35c27fcc123151d81da30
Atakan Korkmaz <ata58kann@gmail.com>
2021-06-22 23:36:19 +0300

    movie detail crash was fixed

--- src/components/pages/MovieDetailpage/index.jsx
+++ src/components/pages/MovieDetailpage/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 /* Dependencies */
 // import env from "react-dotenv";
 import axios from "axios";
@@ -11,18 +11,18 @@ import { useParams } from "react-router";
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-const MovieDetailpage = (props) => {
-  const [movies, setMovies] = [];
+const MovieDetailpage = () => {
+  const [movie, setMovie] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
     axios
       .get(`${API_URL}api/Movies/getbyid?id=${id}`)
-      .then((res) => setMovies(res.data))
+      .then((res) => setMovie(res.data))
       .catch((err) => console.log(err));
   }, []);
 
-  const { name, description, poster } = this.state.movie;
+  let { name, description, poster } = movie;
   return (
     <div>
       <Row>
@@ -48,8 +48,8 @@ const MovieDetailpage = (props) => {
       <hr />
       <Row>
         <Col className="mx-5">
-          <Comments key={uuidv4()} movieid={this.props.match.params.id} />
-          <AddComment key={uuidv4()} movieid={this.props.match.params.id} />
+          <Comments key={uuidv4()} movieid={id} />
+          <AddComment key={uuidv4()} movieid={id} />
         </Col>
       </Row>
     </div>
